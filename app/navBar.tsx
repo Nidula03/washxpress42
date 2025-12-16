39d376d20878e5ba42345317578ea777cfd23152
Nidula Ekanayake <nidulanuwan@gmail.com>
2025-12-17 00:21:33 +0530

    fixed

--- app/navBar.tsx
+++ app/navBar.tsx
@@ -1,14 +1,45 @@
+"use client";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function NavBar() {
+  const pathname = usePathname();
+
+  const handleScroll = (e: React.MouseEvent<HTMLAnchorElement>, id: string) => {
+    if (pathname === "/") {
+      e.preventDefault();
+      const el = document.getElementById(id);
+      if (el) {
+        el.scrollIntoView({ behavior: "smooth" });
+      }
+    }
+  };
   return (
     <nav className="bg-[#0A74DA] px-8 py-6 flex justify-between items-center font-bold text-white">
       <h2 className="m-0 text-xl">WashXpress</h2>
       <div className="flex gap-5">
         <Link href="/" className="text-black">Home</Link>
-        <Link href="/services" className="text-black">Services</Link>
-        <Link href="/#about" className="text-black">About</Link>
-        <Link href="/#subscription-plan" className="text-black">Subscription</Link>
+        <Link
+          href="/#services"
+          className="text-black"
+          onClick={(e) => handleScroll(e, "services")}
+        >
+          Services
+        </Link>
+        <Link
+          href="/#about"
+          className="text-black"
+          onClick={(e) => handleScroll(e, "about")}
+        >
+          About
+        </Link>
+        <Link
+          href="/#subscription-plan"
+          className="text-black"
+          onClick={(e) => handleScroll(e, "subscription-plan")}
+        >
+          Subscription
+        </Link>
         <Link href="/contact" className="text-black">Contact</Link>
       </div>
     </nav>
