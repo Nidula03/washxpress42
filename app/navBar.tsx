3e4fe02ef2db0a272ddf357edd81fc2f1ccebf5b
Nidula Ekanayake <nidulanuwan@gmail.com>
2025-12-16 21:52:12 +0530

    first commit

---
+++ app/navBar.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+
+export default function NavBar() {
+  return (
+    <nav className="bg-[#0A74DA] px-8 py-6 flex justify-between items-center font-bold text-white">
+      <h2 className="m-0 text-xl">WashXpress</h2>
+      <div className="flex gap-5">
+        <Link href="/" className="text-black">Home</Link>
+        <Link href="/services" className="text-black">Services</Link>
+        <Link href="/about" className="text-black">About</Link>
+        <Link href="/subscription-plans" className="text-black">Subscription</Link>
+        <Link href="/contact" className="text-black">Contact</Link>
+      </div>
+    </nav>
+  );
+}
